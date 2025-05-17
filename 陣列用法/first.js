2a8863b20e9a061dc3306c17ab980f105bb297ca
Lance-0410 <zlixin504@gmail.com>
2025-05-17 16:37:13 +0800

    Update first.js

--- 陣列用法/first.js
+++ 陣列用法/first.js
@@ -4,10 +4,11 @@ let mysongs =["wherever you are", "雨愛", "天后", "不能說的秘密","what
 let mysongs =["wherever you are", "雨愛", "天后"];
 mysongs[1] ="夜空最亮的星";
     console.log(mysongs[1]);
-
-    
-mysongs.push("告白氣球");
-    console.log(mysongs);
-
-    mysongs.pop();
-console.log(mysongs);
+shift()	開頭刪元素
+unshift()	開頭加元素  
+push()	尾端加元素
+pop()	尾端刪元素
+splice()	插隊、刪除、替換 splice(2, 1) → 從索引 2（第三個）開始，刪掉 1 個
+join()	轉字串
+indexOf()	找元素位置
+console.log(mysongs.length)	陣列長度
